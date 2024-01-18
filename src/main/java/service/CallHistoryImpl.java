90c09415097ef0feebe38216c598c4d9bcb11942
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-01-18 10:00:00 +0100

    Create Account

--- src/main/java/service/CallHistoryImpl.java
+++ src/main/java/service/CallHistoryImpl.java
@@ -1,30 +1,37 @@
 package service;
 
+import data.models.CallHistory;
 import data.repositories.CallHistoryRepository;
+import data.repositories.ContactRepository;
 import dto.request.CreateAccountRequest;
 import dto.request.LoginRequest;
 import dto.response.CreateAccountResponse;
 import dto.response.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+@Service
 public class CallHistoryImpl implements CallHistoryService {
-    private final CallHistoryRepository implementation;
-
-    public CallHistoryImpl(CallHistoryRepository implementation) {
-        this.implementation = implementation;
+    @Autowired
+    private final CallHistoryRepository repository;
+    public CallHistoryImpl(CallHistoryRepository repository) {
+        this.repository = repository;
     }
-
     @Override
     public CreateAccountResponse createAccount(CreateAccountRequest request) {
         Pattern pattern = Pattern.compile("^(?!\\d+$)\\w{8,20}$");
         Matcher userPassword = pattern.matcher(request.getCreatePassword());
 
         String fullName = request.getFullName();
-        String phoneNumber = request.getPhoneNumber();
         String createPassword = request.getCreatePassword();
         String confirmPassword = request.getConfirmPassword();
+
+        if (userExist(CreateAccountRequest.getPhoneNumber()))
+
+
+        CreateAccuntResponse createAccountResponse (r)
 
 
         if (CreateAccountResponse != null && CreateAccountResponse.isSuccess()) {
@@ -34,7 +41,17 @@ public class CallHistoryImpl implements CallHistoryService {
         }
     }
 
-        String emailAddress = request.getEmailAddress();
+    private boolean userExist(String phoneNumber) {
+        ContactRepository foundUser = repository.findByPhoneNumber(phoneNumber);
+        return foundUser != null;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest request) {
+        return null;
+    }
+
+    String emailAddress = request.getEmailAddress();
         if (isEmailRegisteredBefore(emailAddress)){
             return new CreateAccountResponse("Email already exists. Please, use a different email.");
         }
